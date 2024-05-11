f3dae53e33b1c9dd48d3dd29a308c13149fe3615
Dima <dmitrijs.visnakovs@inbox.lv>
2024-05-11 18:37:16 +0300

    footer

---
+++ app/components/Footer/Footer.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { FaInstagram } from "react-icons/fa6";
+import { FaTelegramPlane } from "react-icons/fa";
+import { RiTiktokLine } from "react-icons/ri";
+
+
+const Footer = () => {
+ 
+  return (
+    <div className="w-full flex justify-center items-center text-[25px] mt-auto h-20 bg-gradient-to-r from-rose-300 to-pink-500">
+      <h2> SOCIĀLIE TĪKLI </h2>
+      <div className="flex gap-1 ml-3 cursor-pointer">
+        <div className="hover:bg-slate-400 rounded-xl p-3">
+          <a
+            target="_blank"
+            href="https://www.instagram.com/volcites_photos?igsh=cnJuam13anZtaGJ0&utm_source=qr"
+          >
+            <FaInstagram />
+          </a>
+        </div>
+        <div className="hover:bg-slate-400 rounded-xl p-3">
+          <a target="_blank" href="https://t.me/lainevolcite">
+            <FaTelegramPlane />
+          </a>
+        </div>
+        <div className="hover:bg-slate-400 rounded-xl p-3">
+          <a
+            target="_blank"
+            href="https://tiktok.com/@volcites_photos?_t=8kGdMatiX4L&_r=1"
+          >
+            <RiTiktokLine />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Footer from "./components/Footer/Footer";
+
 import Header from "./components/Header/Header";
 import "./globals.css";
 
@@ -20,6 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Header />
         {children}
+    <Footer/>
       </body>
     </html>
   );
