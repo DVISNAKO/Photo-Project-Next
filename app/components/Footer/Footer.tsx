cd92b15f43222bfdb419ae5ddb9758677afcbb91
Dima <dmitrijs.visnakovs@inbox.lv>
2024-05-20 16:31:22 +0300

    footer fix

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -11,7 +11,7 @@ const Footer = ({language }:LanguageProps) => {
  
   return (
     <div className="w-full flex justify-center items-center text-[25px] mt-auto h-20 bg-gradient-to-r from-rose-300 to-pink-500">
-      <h2> {language ? <>SOCIĀLIE TĪKLI </>: <>СОЦИАЛЬНЫЕ СЕТИ</>}</h2>
+      <h2 className="flex text-center"> {language ? <>SOCIĀLIE TĪKLI </>: <>СОЦИАЛЬНЫЕ СЕТИ</>}</h2>
       <div className="flex gap-1 ml-3 cursor-pointer">
         <div className="hover:bg-slate-400 rounded-xl p-3">
           <Link
