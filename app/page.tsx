7f9d0d041a569e322aea39ec8f8ce865751f817f
Dima <dmitrijs.visnakovs@inbox.lv>
2024-09-24 07:46:53 +0300

    updade

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import Image from "@/node_modules/next/image";
 import { useContext } from "react";
 import { LanguageState } from "./utils/context";
@@ -8,13 +7,14 @@ import Title from "./components/PageTitle/Title";
 
 export default function GAlerijaPage() {
   const { language, setLanguage } = useContext(LanguageState);
+  const font = { fontFamily: "Playfair Display", fontWeight: 900 };
 
   return (
     <div className="flex justify-center items-center h-screen my-10 flex-wrap gap-5 mx-10 border-black">
       <div className="flex flex-col min-w-[250px] items-center justify-center">
-        <h1 className="es:text-5xl z-10 md:z-10 text-7xl">Laine Volcīte</h1>
+        <h1 className="es:text-5xl z-10 md:text-8xl playfair">Laine Volcīte</h1>
         <Image src={IMG1} alt="foto" className="-rotate-6 w-[450px]" />
-        <h1 className="es:text-5xl z-10 md:z-10 text-7xl">PORTFOLIO</h1>
+        <h1 className="es:text-5xl z-10 md:text-8xl playfair">PORTFOLIO</h1>
         <Title titleLV="FOTOGRĀFS RĒZEKNĒ" titleRus="ФОТОГРАФ ИЗ РЕЗЕКНЕ" />
       </div>
     </div>
